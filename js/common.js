6bd2193c59481f4994d6adf391fc52965c01460c
sundersj <sundersj@outlook.com>
2017-07-02 23:13:45 +0530

    updated version 2

---
+++ js/common.js
@@ -0,0 +1,43 @@
+//#to-top button appears after scrolling
+var fixed = false;
+$(document).scroll(function() {
+	if ($(this).scrollTop() > 250) {
+		if (!fixed) {
+			fixed = true;
+			// $('#to-top').css({position:'fixed', display:'block'});
+			$('#to-top').show("slow", function() {
+				$('#to-top').css({
+					position: 'fixed',
+					display: 'block'
+				});
+			});
+		}
+	} else {
+		if (fixed) {
+			fixed = false;
+			$('#to-top').hide("slow", function() {
+				$('#to-top').css({
+					display: 'none'
+				});
+			});
+		}
+	}
+});
+// Disable Google Maps scrolling
+var onMapMouseleaveHandler = function(event) {
+	var that = $(this);
+	that.on('click', onMapClickHandler);
+	that.off('mouseleave', onMapMouseleaveHandler);
+	that.find('iframe').css("pointer-events", "none");
+}
+var onMapClickHandler = function(event) {
+		var that = $(this);
+		// Disable the click handler until the user leaves the map area
+		that.off('click', onMapClickHandler);
+		// Enable scrolling zoom
+		that.find('iframe').css("pointer-events", "auto");
+		// Handle the mouse leave event
+		that.on('mouseleave', onMapMouseleaveHandler);
+	}
+	// Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler);
