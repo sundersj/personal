ef1f3048a2d1b898cb01ccce804d0253e796e032
sid <sunderjangra789@gmail.com>
2023-08-16 13:50:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App()
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/personal" element={<Home />} />
 				<Route path="portfolio/:id" element={<PortfolioDetail />} />
 				<Route path="*" element={<PageNotfound />} />
 			</Routes>
